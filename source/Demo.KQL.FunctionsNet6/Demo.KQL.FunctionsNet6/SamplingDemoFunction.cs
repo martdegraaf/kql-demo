98cf6abce16b6accdc07b7520a92d1127a72202f
Mart de Graaf <m.degraaf@4dotnet.nl>
2023-02-06 22:43:32 +0100

    update Logger interface

--- source/Demo.KQL.FunctionsNet6/Demo.KQL.FunctionsNet6/SamplingDemoFunction.cs
+++ source/Demo.KQL.FunctionsNet6/Demo.KQL.FunctionsNet6/SamplingDemoFunction.cs
@@ -5,12 +5,19 @@ namespace Demo.KQL.FunctionsNet6
 {
     public class SamplingDemoFunction
     {
+        private readonly ILogger<SamplingDemoFunction> _logger;
+
+        public SamplingDemoFunction(ILogger<SamplingDemoFunction> logger)
+        {
+            _logger = logger;
+        }
+
         [FunctionName("SamplingDemoFunction")]
-        public void Run([TimerTrigger("0 */5 * * * *")] TimerInfo myTimer, ILogger log)
+        public void Run([TimerTrigger("0 */5 * * * *")] TimerInfo myTimer)
         {
             for (int i = 1; i <= 1000; i++)
             {
-                log.LogInformation("Logging messages from {functionName} iteration: {i}", nameof(SamplingDemoFunction), i);
+                _logger.LogInformation("Logging messages from {functionName} iteration: {i}", nameof(SamplingDemoFunction), i);
             }
         }
     }
